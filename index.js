83342a79772f635fc032e51899fa3f19f7adb68c
Yancarlos107 <yancarlosrt842@gmail.com>
2021-07-18 04:06:36 -0500

    Implementacion de los calculos del triaungulo con error en el ultimo else

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ const areaCuadrado = (lado) => {
 
 /* Codigo del triangulo */
 const perimetroTriangulo = (lado1, lado2, base) => {
-    return lado1 + lado2 + base;    
+    return Number(lado1) + Number(lado2) + Number(base);    
 }
 
-const areaTriangulo = (base, altura) => {
-    return (base * altura) / 2;
+const areaTriangulo = (lado1, lado2, base) => {
+    if (lado1 === lado2 && lado2 === base) {
+        altura = Math.sqrt((lado1 ** 2) - ((base/2) ** 2))
+        return ((base * altura) / 2)
+    } if (lado1 === lado2 && lado1 != base) {
+        altura = Math.sqrt((lado1**2) - ((base / 2)** 2) )
+        return ((base * altura) / 2) 
+    } else {
+        const semiperimetro = ((lado1 + lado2 + base) / 2)
+        return Math.sqrt(semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - base))
+    }
 }
 
 console.groupEnd("Triangulos")
@@ -53,4 +62,27 @@ const calcularAreaCuadrado = () => {
 
     const area = areaCuadrado(value);
      alert(area)
+}
+
+const calcularPerimetroTriangulo = () => {
+    const input1 = document.getElementById("InputTriangulo1")
+    const input2 = document.getElementById("InputTriangulo2")
+    const input3 = document.getElementById("InputTriangulo3")
+    const values = [input1.value, input2.value, input3.value]
+
+    const perimetro = perimetroTriangulo(values[0], values[1], values[2])
+    alert(perimetro)
+}
+
+const calcularAreaTriangulo = () => {
+    const input1 = document.getElementById("InputTriangulo1")
+    const input2 = document.getElementById("InputTriangulo2")
+    const input3 = document.getElementById("InputTriangulo3")
+    const value1 = input1.value
+    const value2 = input2.value
+    debugger
+    const value3 = input3.value
+    console.log(value1, value2, value3)
+    const area = areaTriangulo(value1, value2, value3)
+    alert(area)
 }
